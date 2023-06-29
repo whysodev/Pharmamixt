112fa2f8cde37052d2f0f5830070e76f7f219060
whysodev <whyso.developer@gmail.com>
2023-06-29 15:32:57 +0300

    manual page created

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 const time = document.querySelector('.header__nav-time');
 time.innerHTML = `${moment().format("hh:mm, DD MMMM")}`;
 
-const circle = document.querySelector('.speedometer__speedometer-circle');
-const percent = 62;
-const number = 2200 - 1280 * (percent / 100)
+// const circle = document.querySelector('.speedometer__speedometer-circle');
+// const percent2 = 10;
+// const percent = 20;
+// const coordinate = 2200 - 1340 * (percent / 100)
 
-circle.style.strokeDashoffset  = `${number}`
-
+// circle.style.strokeDashoffset = `${coordinate}`
+// 400px - 10
+// 396px
 // 2200 - ((1600 * percentage) / 100)
+const numbers = document.querySelectorAll('.manual__manual-number');
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        numbers.forEach(number => {
+            number.classList.remove('active');
+        })
+        number.classList.add('active')
+    })
+})
