87d9da792ceacc896a5d03637641c4590c3a2795
whysodev <whyso.developer@gmail.com>
2023-06-29 13:17:03 +0300

    info page created

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 const time = document.querySelector('.header__nav-time');
 time.innerHTML = `${moment().format("hh:mm, DD MMMM")}`;
 
+const circle = document.querySelector('.speedometer__speedometer-circle');
+const percent = 62;
+const number = 2200 - 1280 * (percent / 100)
+
+circle.style.strokeDashoffset  = `${number}`
+
+// 2200 - ((1600 * percentage) / 100)
