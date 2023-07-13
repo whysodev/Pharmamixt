0ffdbaae56d7068dfccb80be65c048e23dff54ab
whysodev <whyso.developer@gmail.com>
2023-07-13 08:36:39 +0300

    [fix] time

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const time = document.querySelector('.header__nav-time');
-time.innerHTML = `${moment().format("HH:mm, DD MMMM")}`;
 const language = document.querySelector('.header__nav-language');
+
+function updateTime(){
+    time.innerHTML = `${moment().format("HH:mm, DD MMMM")}`;
+}
+
+updateTime();
+
+setInterval(updateTime, 1000)
 
 language.addEventListener('click', () => {
     if (language.textContent === 'RU') {
